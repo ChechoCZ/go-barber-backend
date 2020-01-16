45f088e9620a079b3b1540c0a446a1cd92cba715
Sergio Cuadros <sergio@sitecbr.com.br>
2020-01-15 22:01:55 -0300

    Added date-fns lib to deal with dates. Added Appointment feature with date and appointment validation.

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 ## Go Barber
 
-This project serves the backend for an online appointment scheduler of a barber shop.
+This project serves the backend for an online appointment for a barber shop.
 
 ### Tech Stack
 This project is being developed using Node.js with Express.js, Sequelize and PostgreSQL.
--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,10 @@ import multer from 'multer';
 import multerConfig from './config/multer'; 
 
 import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth'; 
 
@@ -20,6 +21,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
